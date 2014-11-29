e8a90388ca659f0d60fccd72231418a912200ffe
Railgun3510 <Railgun3510@users.noreply.github.com>
2014-11-29 21:30:12 +0800

    Initial Commit

---
+++ StockHoldings/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StockHoldings
+//
+//  Created by Cheuk Him Ho on 29/11/14.
+//  Copyright (c) 2014 Cheuk Him Ho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
